8dbdcec728a870c838988a11d456a6b4a4fd3887
Cezar Ribeiro <38530653+TyphonBak@users.noreply.github.com>
2018-10-08 20:08:48 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -56,7 +56,7 @@ def cadastro_cursos(request):
         context_instance = RequestContext(request,
         {
             'title':'Cadastro de cursos',
-            'cursos': ['ADS' , 'Sistema de Informacao', 'Administracao'],
+            'cursos': ['ADS' , 'Sistema de Informação', 'Administração'],
             'year':datetime.now().year,
         })
     )
